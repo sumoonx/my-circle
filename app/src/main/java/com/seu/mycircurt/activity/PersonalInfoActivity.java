a0fec51f780de45305911204125008b555e145de
sumoonx <694726071@qq.com>
2015-12-23 21:52:02 +0800

    init git

---
+++ app/src/main/java/com/seu/mycircurt/activity/PersonalInfoActivity.java
@@ -0,0 +1,16 @@
+package com.seu.mycircurt.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.seu.mycircurt.R;
+
+public class PersonalInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info);
+    }
+
+}
