a6175f76282c8deed7ce654f28558afe21e7d67f
evanman <evanman0618@gmail.com>
2016-01-08 14:04:36 +0800

    修改了下底部布局
    图标居中

--- app/src/main/java/com/seu/mycircurt/fragment/ReleaseFragment.java
+++ app/src/main/java/com/seu/mycircurt/fragment/ReleaseFragment.java
@@ -28,6 +28,7 @@ public class ReleaseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_release, container, false);
+
     }
 
 }
